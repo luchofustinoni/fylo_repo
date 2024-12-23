72db95be6678b39f1677bc34c9b1f3f7a7ec77b9
Luciano Fustinoni <lucianofustinoni10@gmail.com>
2024-12-23 13:29:32 -0300

    Mobile finished

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const barsNavbar = document.querySelector(".barsNavbar")
-const navUl = document.querySelector(".nav-ul")
-
-barsNavbar.addEventListener('click', ()=>{
-    navUl.classList.toggle("active")
-})
